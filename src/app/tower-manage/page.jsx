5c35de5af12adb5ed12c4fda311adc89e39d7d06
이예은 <dodomchdodomch@naver.com>
2025-07-01 21:28:35 +0900

    모두가볼수있게되었어

--- src/app/tower-manage/page.jsx
+++ src/app/tower-manage/page.jsx
@@ -1,12 +1,15 @@
 "use client"
 import React, { useState } from "react"
 import NaverMap from "../components/navermap"
+import Menu from "../components/menu"
 
 export default function TowerPage() {
   const [latLng, setLatLng] = useState(null)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <div style={{ maxWidth: 800, margin: "40px auto", padding: 24 }}>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <h2 style={{ marginBottom: 24 }}>타워 위치 관리</h2>
       <NaverMap setLatLng={setLatLng} />
       {latLng && (

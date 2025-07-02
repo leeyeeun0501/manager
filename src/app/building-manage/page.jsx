22912c4062ea0b6af8f346516b6dda7c91bc0686
이예은 <dodomchdodomch@naver.com>
2025-07-02 09:12:11 +0900

    층 텍스트 완료

--- src/app/building-manage/page.jsx
+++ src/app/building-manage/page.jsx
@@ -726,7 +726,7 @@ export default function BuildingPage() {
                   ))}
                 </select>
                 <input
-                  type="number"
+                  type="text"
                   value={addFloorNum}
                   onChange={(e) => setAddFloorNum(e.target.value)}
                   min={1}

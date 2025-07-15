f5d4f05cb11b43d0bd152fc60a05d667c5c57a16
이예은 <dodomchdodomch@naver.com>
2025-07-16 08:49:58 +0900

    오느릐 행운번호

--- src/app/api/apibase.js
+++ src/app/api/apibase.js
@@ -1,3 +1,3 @@
 // src/app/api/_apiBase.js
-export const API_BASE = "http://16.176.161.244:3000" // 일반
-export const AUTH_API_BASE = "http://16.176.161.244:3001" // 회원
+export const API_BASE = "http://3.27.162.178:3000" // 일반
+export const AUTH_API_BASE = "http://3.27.162.178:3001" // 회원

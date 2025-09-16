7b11ea3b93b949ecd104368e10325b085593c251
이예은 <yeeun57795549@gmail.com>
2025-09-16 10:44:30 +0900

    2일차 코드 정리2

--- src/app/api/room-route/[building]/[floor]/route.js
+++ src/app/api/room-route/[building]/[floor]/route.js
@@ -26,54 +26,6 @@ export async function GET(request, { params }) {
   }
 }
 
-// 강의실 추가 (POST)
-// 아마 안 쓸 듯
-export async function POST(req, { params }) {
-  const { building, floor } = params
-  try {
-    const body = await req.json()
-    const { room_name, room_desc, x, y } = body
-
-    if (!room_name || typeof x === "undefined" || typeof y === "undefined") {
-      return NextResponse.json({ error: "필수 항목 누락" }, { status: 400 })
-    }
-
-    const res = await fetch(
-      `${API_BASE}/room/${encodeURIComponent(building)}/${encodeURIComponent(
-        floor
-      )}`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          room_name,
-          room_desc,
-          x,
-          y,
-        }),
-      }
-    )
-
-    if (!res.ok) {
-      let data = {}
-      try {
-        data = await res.json()
-      } catch {}
-      return NextResponse.json(
-        { error: data.error || "DB 오류" },
-        { status: 500 }
-      )
-    }
-
-    const data = await res.json().catch(() => ({}))
-    return NextResponse.json({
-      message: data.message || "방 추가가 완료되었습니다",
-    })
-  } catch (err) {
-    return NextResponse.json({ error: "서버 오류" }, { status: 500 })
-  }
-}
-
 // 수정 (PUT)
 export async function PUT(req, context) {
   const params = (await context.params) ?? {}
@@ -124,6 +76,55 @@ export async function PUT(req, context) {
     return NextResponse.json({ error: "서버 오류" }, { status: 500 })
   }
 }
+
+// 강의실 추가 (POST)
+// 못 씀
+export async function POST(req, { params }) {
+  const { building, floor } = params
+  try {
+    const body = await req.json()
+    const { room_name, room_desc, x, y } = body
+
+    if (!room_name || typeof x === "undefined" || typeof y === "undefined") {
+      return NextResponse.json({ error: "필수 항목 누락" }, { status: 400 })
+    }
+
+    const res = await fetch(
+      `${API_BASE}/room/${encodeURIComponent(building)}/${encodeURIComponent(
+        floor
+      )}`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          room_name,
+          room_desc,
+          x,
+          y,
+        }),
+      }
+    )
+
+    if (!res.ok) {
+      let data = {}
+      try {
+        data = await res.json()
+      } catch {}
+      return NextResponse.json(
+        { error: data.error || "DB 오류" },
+        { status: 500 }
+      )
+    }
+
+    const data = await res.json().catch(() => ({}))
+    return NextResponse.json({
+      message: data.message || "방 추가가 완료되었습니다",
+    })
+  } catch (err) {
+    return NextResponse.json({ error: "서버 오류" }, { status: 500 })
+  }
+}
+
 
 // 강의실 삭제 (DELETE)
 // 아직 안 씀

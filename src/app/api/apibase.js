19382bb2474354721e6aba9e1c0d6ffe1beba7df
이예은 <yeeun57795549@gmail.com>
2025-09-24 13:02:05 +0900

    다시 햄부기 바낌

--- src/app/api/apibase.js
+++ src/app/api/apibase.js
@@ -1,3 +1,3 @@
 // apibase
-export const API_BASE = "http://3.104.74.109:3000" // 일반
+export const API_BASE = "http:/3.104.74.109:3000" // 일반
 export const AUTH_API_BASE = "http://3.104.74.109:3001" // 회원

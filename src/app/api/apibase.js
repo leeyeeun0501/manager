774f36e2c0582458ba48beeb0c97d55f53e401bc
이예은 <yeeun57795549@gmail.com>
2025-12-04 16:05:50 +0900

    https

--- src/app/api/apibase.js
+++ src/app/api/apibase.js
@@ -1,3 +1,3 @@
 // apibase
-export const API_BASE = "http://52.65.94.225:3000" // 일반
-export const AUTH_API_BASE = "http://52.65.94.225:3000" // 회원
+export const API_BASE = "https://52.65.94.225:3000" // 일반
+export const AUTH_API_BASE = "https://52.65.94.225:3000" // 회원
